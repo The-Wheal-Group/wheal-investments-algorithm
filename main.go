9792a9e627354cf0d61c1139a8f2935dc9ff597b
John Wheal <john@wheal.co.uk>
2020-04-12 12:48:58 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fund1 := []int{20, 40, 60}
+	fund2 := []int{30, 10, 60}
+
+	desired := []int{30, 10, 60}
+
+	fmt.Println(fund1)
+	fmt.Println(fund2)
+	fmt.Println(desired)
+}
