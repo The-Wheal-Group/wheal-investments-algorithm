e63022c29ac9af03490bac271f40435936ba477f
John Wheal <john@wheal.co.uk>
2020-04-12 15:04:55 +0100

    Added acutal and desired allocation scoring

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	//fund1 := [3]int{20, 40, 60}
-	//fund2 := [3]int{30, 10, 60}
-	desired := [3]int{30, 10, 60}
+	allocation := [3]float64{0.0, 0.9, 0.1}
 
-	allocation := [3]int{30, 20, 50}
-
-	scoreAllocation(allocation, desired)
+	scoreAllocation(allocation)
 }
 
-func scoreAllocation(allocation [3]int, desired [3]int) {
+func scoreAllocation(allocation [3]float64) {
+	fund1 := [3]float64{20, 40, 60}
+	fund2 := [3]float64{30, 10, 60}
+	fund3 := [3]float64{60, 20, 20}
+	funds := [3][3]float64{fund1, fund2, fund3}
+	desiredAllocation := [3]float64{30, 10, 60}
 
-	difference := 0
+	var fundsAllocation [3][3]float64
 
-	for index, value := range desired {
-		if value > allocation[index] {
-			difference += value - allocation[index]
-		} else {
-			difference += allocation[index] - value
+	for regionsIndex := 0; regionsIndex < 3; regionsIndex++ {
+		for fundsIndex := 0; fundsIndex < 3; fundsIndex++ {
+			fundsAllocation[regionsIndex][fundsIndex] = funds[regionsIndex][fundsIndex] * allocation[regionsIndex]
 		}
 	}
+
+	var actualAllocation [3]float64
+
+	for regionsIndex := 0; regionsIndex < 3; regionsIndex++ {
+		for fundsIndex := 0; fundsIndex < 3; fundsIndex++ {
+			actualAllocation[regionsIndex] += fundsAllocation[fundsIndex][regionsIndex]
+		}
+	}
+
+	difference := 0.0
+
+	for index := 0; index < 3; index++ {
+		difference += math.Abs(desiredAllocation[index] - actualAllocation[index])
+	}
+
+	fmt.Println(difference)
 }
